f60ac25e8630005f547fb154cd4fa152d6356d2e
romarakhlin <rrakhlin@gmail.com>
2019-11-09 13:00:29 +0300

    get users location

--- noName/ViewController.swift
+++ noName/ViewController.swift
@@ -9,26 +9,39 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, CLLocationManagerDelegate {
+   
     let locationManager = CLLocationManager()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+     override func viewDidLoad() {
+         super.viewDidLoad()
         
-        func setupLocationManager() {
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
-            locationManager.requestWhenInUseAuthorization()
-            locationManager.startUpdatingLocation()
-        }
-        
-        func locationManager(_ manager: CLL
-            
+        setupLocationManager()
+     }
+
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            if location.horizontalAccuracy > 0 {
+                locationManager.stopUpdatingLocation()
+                
+                let longitude = location.coordinate.longitude
+                let latitude = location.coordinate.latitude
+                
+                print("longitude = \(longitude), latitude = \(latitude)")
+            }
         }
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location update failed, \(error)")
+    }
 }
 
 
