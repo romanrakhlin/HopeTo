55aa2d045e653dfdba2694e119fa34e7631623b9
Murat <89196242960@mail.ru>
2019-11-09 12:21:43 +0300

    firstUpd

---
+++ noName/MapViewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewViewController.swift
+//  noName
+//
+//  Created by Anmin on 11/9/19.
+//  Copyright © 2019 Roman Rakhlin. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
+
+extension MapViewViewController{
+    
+}
